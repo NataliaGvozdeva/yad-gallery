fe0a3fd82c217c0171c49703bb4fcae3ff31e61c
Dmitry Mersiyanov <dmersianov@ostrovok.ru>
2018-04-28 15:58:59 +0300

    add working viewpager with toolbar

--- app/src/main/java/com/mersiyanov/dmitry/yadg/FullPicFragment.java
+++ app/src/main/java/com/mersiyanov/dmitry/yadg/FullPicFragment.java
@@ -1,10 +1,11 @@
 package com.mersiyanov.dmitry.yadg;
 
 
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,99 +16,38 @@ import com.squareup.picasso.Picasso;
 
 public class FullPicFragment extends Fragment {
 
-    private ViewPager mViewPager;
-    ImageView imageView;
-
+    private ImageView imageView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_blank, container, false);
+        View view = inflater.inflate(R.layout.fragment_full_pic, container, false);
+        imageView = view.findViewById(R.id.full_pic);
 
+        Toolbar mActionBarToolbar = view.findViewById(R.id.toolbar);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.setSupportActionBar(mActionBarToolbar);
+        activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        activity.getSupportActionBar().setTitle("Test");
+        mActionBarToolbar.getNavigationIcon().setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_ATOP);
 
-
-        return v;
+        return view;
     }
 
-    public static FullPicFragment newInstance(String pictureUrl) {
+    public static FullPicFragment newInstance(int position, String imageUrl) {
 
         FullPicFragment f = new FullPicFragment();
         Bundle b = new Bundle();
-        b.putString("pictureUrl", pictureUrl);
-
+        b.putString("pictureUrl", imageUrl);
+        b.putInt("pos", position);
         f.setArguments(b);
 
         return f;
     }
 
-
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
 
-        mViewPager = view.findViewById(R.id.viewpager);
-        mViewPager.setAdapter(new SamplePagerAdapter());
-
-
-
-    }
-
-    class SamplePagerAdapter extends PagerAdapter {
-
-        /**
-         * @return the number of pages to display
-         */
-        @Override
-        public int getCount() {
-
-            return MainActivity.itemList.size();
-        }
-
-        /**
-         * @return true if the value returned from {@link #instantiateItem(ViewGroup, int)} is the
-         * same object as the {@link View} added to the {@link ViewPager}.
-         */
-        @Override
-        public boolean isViewFromObject(View view, Object o) {
-            return o == view;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return "Item " + (position + 1);
-        }
-
-        /**
-         * Instantiate the {@link View} which should be displayed at {@code position}. Here we
-         * inflate a layout from the apps resources and then change the text view to signify the position.
-         */
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            // Inflate a new layout from our resources
-            View view = getActivity().getLayoutInflater().inflate(R.layout.pager_item, container, false);
-            // Add the newly created View to the ViewPager
-            container.addView(view);
-
-            // Retrieve a TextView from the inflated View, and update it's text
-
-            imageView = view.findViewById(R.id.full_pic);
-            Picasso.get().load(getArguments().getString("pictureUrl")).into(imageView);
-            Picasso.get().load(MainActivity.itemList.get(position).getFile()).into(imageView);
-
-
-            // Return the View
-            return view;
-        }
-
-
-
-        /**
-         * Destroy the item from the {@link ViewPager}. In our case this is simply removing the
-         * {@link View}.
-         */
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            container.removeView((View) object);
-
-        }
+        Picasso.get().load(MainActivity.itemList.get(getArguments().getInt("pos")).getFile()).into(imageView);
 
     }
 }
--- app/src/main/java/com/mersiyanov/dmitry/yadg/FullPictureActivity.java
+++ app/src/main/java/com/mersiyanov/dmitry/yadg/FullPictureActivity.java
@@ -2,10 +2,20 @@ package com.mersiyanov.dmitry.yadg;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 public class FullPictureActivity extends AppCompatActivity {
+
+    private static final int NUM_PAGES = 5;
+
+    private PagerAdapter mPagerAdapter;
+    private ViewPager mPager;
+    private String imageUrl;
 
 
     @Override
@@ -14,23 +24,28 @@ public class FullPictureActivity extends AppCompatActivity {
         setContentView(R.layout.activity_full_picture);
 
         Intent intent = getIntent();
-        String imageUrl = intent.getStringExtra("pic_url");
+        imageUrl = intent.getStringExtra("pic_url");
 
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        FullPicFragment fragment = FullPicFragment.newInstance(imageUrl);
-        transaction.replace(R.id.sample_content_fragment, fragment);
-        transaction.commit();
+        mPager = findViewById(R.id.pager);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+
+    }
 
 
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
 
-//        imageView = findViewById(R.id.full_pic);
-//
-//        Picasso.get().load(imageUrl).into(imageView);
-//
-//        Toolbar mActionBarToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
-//        setSupportActionBar(mActionBarToolbar);
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//        getSupportActionBar().setTitle("Test");
+        @Override
+        public Fragment getItem(int position) {
+            return FullPicFragment.newInstance(position, imageUrl);
+        }
 
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
     }
 }
