314c2ae264e620fb4d1b229f355cdb06e35de4a7
Dmitry Mersiyanov <dmersiyanov@yandex.ru>
2018-05-13 23:03:53 +0300

    fixed back button on fragment

--- app/src/main/java/com/mersiyanov/dmitry/yadg/ui/FullPicFragment.java
+++ app/src/main/java/com/mersiyanov/dmitry/yadg/ui/FullPicFragment.java
@@ -1,12 +1,7 @@
 package com.mersiyanov.dmitry.yadg.ui;
 
-import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,20 +13,20 @@ import com.mersiyanov.dmitry.yadg.pojo.Item;
 import com.squareup.leakcanary.RefWatcher;
 import com.squareup.picasso.Picasso;
 
-import java.util.Formatter;
-
 import io.realm.RealmList;
 
 public class FullPicFragment extends Fragment {
 
     private static RealmList<Item> itemRealmList;
     private ImageView imageView;
-    private Toolbar mActionBarToolbar;
-    private ViewPager mViewPager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_full_pic, container, false);
+        View view = inflater.inflate(R.layout.fragment_full_pic, container, false);
+        imageView = view.findViewById(R.id.full_pic);
+        Picasso.get().load(itemRealmList.get(getArguments().getInt("position")).getFile()).into(imageView);
+        return view;
+
     }
 
     public static FullPicFragment newInstance(int position, RealmList<Item> itemList) {
@@ -41,59 +36,6 @@ public class FullPicFragment extends Fragment {
         b.putInt("position", position);
         fragment.setArguments(b);
         return fragment;
-    }
-
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        mViewPager = view.findViewById(R.id.viewpager);
-        mViewPager.setAdapter(new SamplePagerAdapter());
-        mViewPager.setCurrentItem(getArguments().getInt("position"));
-    }
-
-    class SamplePagerAdapter extends PagerAdapter {
-
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            View view = getActivity().getLayoutInflater().inflate(R.layout.pager_item, container, false);
-
-            imageView = view.findViewById(R.id.full_pic);
-            mActionBarToolbar = view.findViewById(R.id.toolbar);
-
-            Picasso.get().load(itemRealmList.get(position).getFile()).into(imageView);
-            initActionBar(position);
-
-            container.addView(view);
-            return view;
-        }
-
-        private void initActionBar(int position) {
-            AppCompatActivity activity = (AppCompatActivity) getActivity();
-            activity.setSupportActionBar(mActionBarToolbar);
-            activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-            Formatter f = new Formatter();
-            f.format("%d из %d", position + 1,  itemRealmList.size());
-
-            activity.getSupportActionBar().setTitle(f.toString());
-            mActionBarToolbar.getNavigationIcon().setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_ATOP);
-
-        }
-
-        @Override
-        public int getCount() {
-            return itemRealmList.size();
-        }
-
-        @Override
-        public boolean isViewFromObject(View view, Object o) {
-            return o == view;
-        }
-
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            container.removeView((View) object);
-
-        }
     }
 
     @Override
--- app/src/main/java/com/mersiyanov/dmitry/yadg/ui/FullPictureActivity.java
+++ app/src/main/java/com/mersiyanov/dmitry/yadg/ui/FullPictureActivity.java
@@ -1,12 +1,19 @@
 package com.mersiyanov.dmitry.yadg.ui;
 
-import android.content.Intent;
+import android.annotation.SuppressLint;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.mersiyanov.dmitry.yadg.R;
 import com.mersiyanov.dmitry.yadg.pojo.Item;
+
+import java.util.Formatter;
 
 import io.realm.Realm;
 import io.realm.RealmList;
@@ -16,7 +23,10 @@ public class FullPictureActivity extends AppCompatActivity {
 
     private RealmList<Item> itemRealmList;
     private Realm realm;
+    private Toolbar actionBarToolbar;
+    private ViewPager viewPager;
 
+    @SuppressLint("RestrictedApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -27,14 +37,45 @@ public class FullPictureActivity extends AppCompatActivity {
 
         getPicturesFromDb();
 
-        Intent intent = getIntent();
-        int position = intent.getIntExtra("pic_position", 0);
+        int position = getIntent().getIntExtra("pic_position", 0);
+        initViewPager(position);
+        initActionBar(position);
 
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        FullPicFragment fragment = FullPicFragment.newInstance(position, itemRealmList);
-        transaction.addToBackStack(null);
-        transaction.replace(R.id.content_fragment, fragment);
-        transaction.commit();
+    }
+
+    private void initViewPager(int position) {
+        viewPager = findViewById(R.id.viewpager);
+        viewPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
+        viewPager.setCurrentItem(position);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                Formatter f = new Formatter();
+                f.format("%d из %d", position + 1,  itemRealmList.size());
+                getSupportActionBar().setTitle(f.toString());
+            }
+
+            @Override
+            public void onPageSelected(int position) {}
+
+            @Override
+            public void onPageScrollStateChanged(int state) {}
+        });
+    }
+
+    @SuppressLint("RestrictedApi")
+    private void initActionBar(int position) {
+
+        actionBarToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(actionBarToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+
+        Formatter f = new Formatter();
+        f.format("%d из %d", position + 1,  itemRealmList.size());
+        getSupportActionBar().setTitle(f.toString());
+
+        actionBarToolbar.getNavigationIcon().setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_ATOP);
 
     }
 
@@ -48,5 +89,24 @@ public class FullPictureActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         realm.close();
+    }
+
+
+    class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return FullPicFragment.newInstance(position, itemRealmList);
+        }
+
+        @Override
+        public int getCount() {
+            return itemRealmList.size();
+
+        }
     }
 }
